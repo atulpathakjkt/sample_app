d7ba5b9ed84e867d063e58eda8b5147608eac8fd
atul <atul@localhost.(none)>
2014-03-28 15:31:29 +0530

    Full fletched Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '1ba2bd07252bbb6cb4518ea5d17730736eb2760162dbb9631b9ed81ec01536a48f7abd3f4d29e836f394641b9601e53a0df5f2f5af53b992156feb6546e80441'
